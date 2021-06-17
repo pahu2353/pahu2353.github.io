07c4fa88a383660d5917eb74ad1ec845ecbe137f
pahu2353 <50181097+pahu2353@users.noreply.github.com>
2021-06-17 01:48:31 -0700

    Add files via upload

---
+++ Patrick-FactFinder/script.js
@@ -0,0 +1,20 @@
+function fact1() {
+	document.getElementById("b").innerHTML = "<img src='images/mcdonalds.webp' alt='mcdonalds'> </img>" + "<br><br>McDonald's has made bubble-gum flavoured broccoli before. Ew."  
+}
+
+function fact2() {
+	document.getElementById("b").innerHTML = "<img src='images/moon.jpeg' alt='moon'></img>" + "<br><br>There used to be volcanoes on the moon."
+}
+
+function fact3() {
+	document.getElementById("b").innerHTML = "<img src='images/nose.jpeg' alt='nose'></img>" + "<br><br>Noses and ears never stop growing."
+}
+
+function fact4() {
+	document.getElementById("b").innerHTML = "<img src='images/rats.jpeg' alt='rats'></img>" + "<br><br>Rats can have dreams."
+}
+
+function fact5() {
+	document.getElementById("b").innerHTML = "<img src='images/jump.jpeg' alt='jump'></img>" + "<br><br>The human long jump world record is greater than the horse long jump world record."
+}
+
